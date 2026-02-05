5250c368a14f34ec08c562601eb9c16a8c21e994
Kingshuk Mondal <135739510+kinghsauk0@users.noreply.github.com>
2026-02-05 14:06:36 +0530

    from king

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -6,9 +6,7 @@ import "primereact/resources/themes/bootstrap4-light-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import 'primeicons/primeicons.css';
 import { PrimeReactProvider } from "primereact/api";
-import "primereact/resources/themes/lara-light-blue/theme.css";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
+
 
 
 const geistSans = Geist({
